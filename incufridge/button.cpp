784675ea688d0523ccf60d1df62054fef23f5216
kiyun-k <kiywoo4ever@gmail.com>
2013-11-03 10:27:33 -0500

    Update button.cpp

--- incufridge/button.cpp
+++ incufridge/button.cpp
@@ -1,10 +1,8 @@
 #include "project.h"
 
-Button::Button(int pin, int holdtime, int level)
+Button::Button(int pin, int level)
 { 
   myPin = pin;
-  pinMode(myPin, OUTPUT);
-  myHoldTime = holdtime; // at least 2.5 seconds
   pressLevel = level;
   if(pressLevel == HIGH){
     releaseLevel = LOW;
@@ -13,18 +11,33 @@ Button::Button(int pin, int holdtime, int level)
     releaseLevel = HIGH;
   }
 }
-void Button::myPress(int holdtime)
-{
+
+void Button::HoldUntilTime(int holdtime){ // at least 2.5 seconds
   digitalWrite(myPin, pressLevel);
   delay(holdtime);
   digitalWrite(myPin, releaseLevel);
-  delay(holdtime);
 }
-void Button::Press()
-{
-  myPress(myHoldTime);
+
+void Button::HoldUntilTemp(String temperature){
+  digitalWrite(myPin, pressLevel);
+  String temp = 0;
+  while(!temp.equals(temperature)){
+    temp = readDisplay();
+  }
 }
-void Button::Hold(int holdtime)
-{
-  myPress(holdtime);
-}
+  
+//void Button::myPress(int holdtime)
+//{
+//  digitalWrite(myPin, pressLevel);
+//  delay(holdtime);
+//  digitalWrite(myPin, releaseLevel);
+//  delay(holdtime);
+//}
+//void Button::Press()
+//{
+//  myPress(myHoldTime);
+//}
+//void Button::Hold(int holdtime)
+//{
+//  myPress(holdtime);
+//}
