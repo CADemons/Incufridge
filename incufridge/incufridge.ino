5469e91e9a9355d029f615b04b51aff5ba2eb597
kiyun-k <kiywoo4ever@gmail.com>
2013-11-03 10:29:42 -0500

    Update incufridge.ino

--- incufridge/incufridge.ino
+++ incufridge/incufridge.ino
@@ -1,4 +1,38 @@
 #include "project.h"
+const int ButtonHoldTime = 2500;
+Button light(SimpleLight, ButtonHoldTime, 1);
+// Pwm myPwm = Pwm(3, 5, 0.8, 0.2, PWM_LOW);
+void setup(){
+//  pinMode(3, OUTPUT);
+//  myPwm.Start();
+//  Serial.begin(9600);
+  pinMode(SimpleLight, OUTPUT);
+  pinMode(trans1, INPUT);
+  pinMode(trans2, INPUT);
+  pinMode(trans3, INPUT);
+  pinMode(read1, INPUT);
+  pinMode(read2, INPUT);
+  pinMode(read3, INPUT);
+  pinMode(read4, INPUT);
+  pinMode(read5, INPUT);
+  pinMode(read6, INPUT);
+  pinMode(read7, INPUT);
+  pinMode(read8, INPUT);
+  
+
+}
+void loop(){
+//  light.Press();
+//  myPwm.Update();
+ // digitalWrite(3, HIGH);
+  //delay(1000);
+ // digitalWrite(3, LOW);
+  //delay(1000);
+}
+
+
+/* for integration with GUI
+#include "project.h"
 const int ButtonHoldTime = 250;
 int temps[12];
 boolean recieving=false;
@@ -41,4 +75,4 @@ void serialEvent(){
     Serial.println('R');
     recieving=false;
   }
-}
+}*/
