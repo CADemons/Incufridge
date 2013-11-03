fb10cb88a005ee0d66b174e04c0eaffa10ffd26d
kiyun-k <kiywoo4ever@gmail.com>
2013-11-03 10:27:01 -0500

    Update button.h

--- incufridge/button.h
+++ incufridge/button.h
@@ -1,43 +1,13 @@
-#include "project.h"
-
-Button::Button(int pin, int level)
-{ 
-  myPin = pin;
-  pressLevel = level;
-  if(pressLevel == HIGH){
-    releaseLevel = LOW;
-  }
-  else{
-    releaseLevel = HIGH;
-  }
-}
-
-void Button::HoldUntilTime(int holdtime){ // at least 2.5 seconds
-  digitalWrite(myPin, pressLevel);
-  delay(holdtime);
-  digitalWrite(myPin, releaseLevel);
-}
-
-void Button::HoldUntilTemp(String temperature){
-  digitalWrite(myPin, pressLevel);
-  String temp = 0;
-  while(!temp.equals(temperature)){
-    temp = readDisplay();
-  }
-}
-  
-//void Button::myPress(int holdtime)
-//{
-//  digitalWrite(myPin, pressLevel);
-//  delay(holdtime);
-//  digitalWrite(myPin, releaseLevel);
-//  delay(holdtime);
-//}
-//void Button::Press()
-//{
-//  myPress(myHoldTime);
-//}
-//void Button::Hold(int holdtime)
-//{
-//  myPress(holdtime);
-//}
+class Button{
+  public: 
+  Button(int pin, int level = HIGH);
+  void Press();
+  void HoldUntilTime(int holdtime);
+  void HoldUntilTemp(String temperature);
+  void Release();
+  private:
+  int myPin;
+  int pressLevel;
+  int releaseLevel;
+//  void myPress(int holdtime);
+};
