97e79a77ea4dd77584e454486eceffbf3cb8b2dd
LincB <lincb123@gmail.com>
2013-11-10 10:53:01 -0500

    Changed dependency order

--- incufridge/project.h
+++ incufridge/project.h
@@ -1,12 +1,15 @@
 #include "Arduino.h"
-#include "button.h"
 #include "mega_arduino_pinassignments.h"
+#include "time.h"
+#include "CommandProcessor.h"
+#include "temperature.h"
+#include "button.h"
 #include "thermistor.h"
 #include "PWM.h" 
 #include "fan.h"
 #include "light.h"
-#include "time.h"
-#include "temperature.h"
-#include "CommandProcessor.h"
 
-
+/*
+Version history:
+1. Moved time, CommandProcessor, pinassignments, and temperature to the top -Linc
+*/
