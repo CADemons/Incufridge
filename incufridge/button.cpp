40f92007a2128a44f4ab1a42754790b29bd9f957
kiyun-k <kiywoo4ever@gmail.com>
2013-11-24 10:46:56 -0500

    Update button.cpp

--- incufridge/button.cpp
+++ incufridge/button.cpp
@@ -1,43 +1,100 @@
+//**************************************************************************
+//
+//				COPYRIGHT NOTICE
+//			Copyright 2013 CONCORD ACADEMY
+//		This program is the property of CONCORD ACADEMY
+//		Any unauthorized use or duplication is prohibited
+//
+//**************************************************************************
+//
+//  Title		Button Implementation
+//  Filename		button.cpp
+//  Originator  	Kiyun Kim  
+//  Archive Location	(wherever this is on github)
+//
+//  Overview		Allows user to press and release a button. 
+//
+//**************************************************************************
+
 #include "project.h"
 
-Button::Button(int pin, int level)
-{ 
-  myPin = pin;
-  pressLevel = level;
-  if(pressLevel == HIGH){
-    releaseLevel = LOW;
+Button::Button(const int Pin, int msPressTime, const int PressLevel){ 
+  m_Pin = Pin;
+  m_PressLevel = PressLevel;
+  if(m_PressLevel == c_LEVEL_HIGH){
+    m_ReleaseLevel = c_LEVEL_LOW;
   }
   else{
-    releaseLevel = HIGH;
+    m_ReleaseLevel = c_LEVEL_HIGH;
   }
 }
 
-void Button::HoldUntilTime(int holdtime){ // at least 2.5 seconds
-  digitalWrite(myPin, pressLevel);
-  delay(holdtime);
-  digitalWrite(myPin, releaseLevel);
+
+/* This method initializes the pin associated with the button.
+ It will set each pin as an output. */
+void Button::Initialize(){
+  pinMode(m_Pin, OUTPUT);
 }
 
-void Button::HoldUntilTemp(String temperature){
-  digitalWrite(myPin, pressLevel);
-  String temp = 0;
-  while(!temp.equals(temperature)){
-    //temp = readDisplay();
+
+/* This method will press a button until a function that releases
+ the button is called. It will not block other functions.*/
+void Button::Hold(){
+  if(IsUp()){
+    digitalWrite(m_Pin, c_LEVEL_HIGH); 
+  } 
+}
+
+
+/* This method will press a button and hold for a given number of
+ seconds (number of seconds must exceed 2.5 seconds). It will block 
+ all other functions.*/
+void Button::HoldFor(int sHoldTime){ 
+  digitalWrite(m_Pin, m_PressLevel);
+  delay(sHoldTime);
+  digitalWrite(m_Pin, m_ReleaseLevel);
+}
+
+
+/* This method releases a pressed button. It is paired with the Hold
+ method. */
+void Button::Release(){
+  if(IsDown()){
+    digitalWrite(m_Pin, c_LEVEL_LOW);
   }
 }
-  
-//void Button::myPress(int holdtime)
-//{
-//  digitalWrite(myPin, pressLevel);
-//  delay(holdtime);
-//  digitalWrite(myPin, releaseLevel);
-//  delay(holdtime);
-//}
-//void Button::Press()
-//{
-//  myPress(myHoldTime);
-//}
-//void Button::Hold(int holdtime)
-//{
-//  myPress(holdtime);
-//}
+
+
+/* This method checks to see if a given pin is in the LOW state (c_LEVEL_LOW). 
+ If this is true, then the button is not currently pressed, and 
+ the function returns true. */
+boolean Button::IsUp(){
+  if(digitalRead(m_Pin) == m_ReleaseLevel){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+
+/* This method checks to see if a given pin is in the HIGH state (c_LEVEL_HIGH). 
+ If this is true, then the button is currently pressed, and the function
+ returns true. */
+boolean Button::IsDown(){
+  if(digitalRead(m_Pin) == m_PressLevel){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+//**************************************************************************
+//
+//  REVISION HISTORY:
+//
+//
+//
+//**************************************************************************
+
