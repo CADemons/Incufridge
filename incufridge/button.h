b1bd69df13b955a5e2dd40e16cce999a545f412d
kiyun-k <kiywoo4ever@gmail.com>
2013-11-24 10:46:31 -0500

    Update button.h

--- incufridge/button.h
+++ incufridge/button.h
@@ -1,18 +1,48 @@
-#ifndef BUTTON_H
+//**************************************************************************
+//
+//				COPYRIGHT NOTICE
+//			Copyright 2013 CONCORD ACADEMY
+//		This program is the property of CONCORD ACADEMY
+//		Any unauthorized use or duplication is prohibited
+//
+//**************************************************************************
+//
+//  Title		Button Class
+//  Filename		button.h
+//  Originator  	Kiyun Kim 
+//  Archive Location	- (wherever this is on  github)
+//
+//  Overview		Provides framework for the Button class, which allows
+//                      the user to press and release buttons.
+//**************************************************************************
+
+#ifndef  BUTTON_H
 #define BUTTON_H
 
 class Button{
-  public: 
-  Button(int pin, int level = HIGH);
-  void Press();
-  void HoldUntilTime(int holdtime);
-  void HoldUntilTemp(String temperature);
+public: 
+  Button(const int Pin, int msPressTime, const int PressLevel = c_LEVEL_HIGH);
+  void Initialize();
+  void Hold();
+  void HoldFor(int HoldTime);
   void Release();
-  private:
-  int myPin;
-  int pressLevel;
-  int releaseLevel;
-//  void myPress(int holdtime);
+  boolean IsUp();
+  boolean IsDown();
+private:
+  int m_Pin;
+  int m_PressLevel;
+  int m_ReleaseLevel;
 };
-
 #endif
+//**************************************************************************
+//
+//  REVISION HISTORY:
+//
+//
+//
+//**************************************************************************
+
+
+
+
+
