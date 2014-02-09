3d2bbfbd17d09550b8a8c305d3f8a2e5f318f64d
kiyun-k <kiywoo4ever@gmail.com>
2014-02-09 10:55:55 -0500

    Update mega_arduino_pinassignments.h

--- incufridge/mega_arduino_pinassignments.h
+++ incufridge/mega_arduino_pinassignments.h
@@ -1,106 +1,137 @@
-#ifndef MEGA_ARDUINO-PINASSIGNMENTS-H
-#define MEGA_ARDUINO-PINASSIGNMENTS-H
+//**************************************************************************
+//
+//				COPYRIGHT NOTICE
+//			Copyright 2013 CONCORD ACADEMY
+//		This program is the property of CONCORD ACADEMY
+//		Any unauthorized use or duplication is prohibited
+//
+//**************************************************************************
+//
+//  Title		Pin Assignments
+//  Filename		mega_arduino_pinassignments.h
+//  Originator  	Kiyun Kim 
+//  Archive Location	- (wherever this is on  github)
+//
+//  Overview		Contains a list of all possible pins on the Arduino
+//                      Mega 2560 and constants assigning pins to particular
+//                      functions.
+//**************************************************************************
 
-const int AnalogPin0 = 0;
-const int AnalogPin1 = 1;
-const int AnalogPin2 = 2;
-const int AnalogPin3 = 3;
-const int AnalogPin4 = 4;
-const int AnalogPin5 = 5;
-const int AnalogPin6 = 6;
-const int AnalogPin7 = 7;
-const int AnalogPin8 = 8;
-const int AnalogPin9 = 9;
-const int AnalogPin10 = 10;
-const int AnalogPin11 = 11;
-const int AnalogPin12 = 12;
-const int AnalogPin13 = 13;
-const int AnalogPin14 = 14;
-const int AnalogPin15 = 15;
-const int DigitalPin0 = 0;
-const int DigitalPin1 = 1;
-const int DigitalPin2 = 2;
-const int DigitalPin3 = 3;
-const int DigitalPin4 = 4;
-const int DigitalPin5 = 5;
-const int DigitalPin6 = 6;
-const int DigitalPin7 = 7;
-const int DigitalPin8 = 8;
-const int DigitalPin9 = 9;
-const int DigitalPin10 = 10;
-const int DigitalPin11 = 11;
-const int DigitalPin12 = 12;
-const int DigitalPin13 = 13;
-const int DigitalPin14 = 14;
-const int DigitalPin15 = 15;
-const int DigitalPin16 = 16;
-const int DigitalPin17 = 17;
-const int DigitalPin18 = 18;
-const int DigitalPin19 = 19;
-const int DigitalPin20 = 20;
-const int DigitalPin21 = 21;
-const int DigitalPin22 = 22;
-const int DigitalPin23 = 23;
-const int DigitalPin24 = 24;
-const int DigitalPin25 = 25;
-const int DigitalPin26 = 26;
-const int DigitalPin27 = 27;
-const int DigitalPin28 = 28;
-const int DigitalPin29 = 29;
-const int DigitalPin30 = 30;
-const int DigitalPin31 = 31;
-const int DigitalPin32 = 32;
-const int DigitalPin33 = 33;
-const int DigitalPin34 = 34;
-const int DigitalPin35 = 35;
-const int DigitalPin36 = 36;
-const int DigitalPin37 = 37;
-const int DigitalPin38 = 38;
-const int DigitalPin39 = 39;
-const int DigitalPin40 = 40;
-const int DigitalPin41 = 41;
-const int DigitalPin42 = 42;
-const int DigitalPin43 = 43;
-const int DigitalPin44 = 44;
-const int DigitalPin45 = 45;
-const int DigitalPin46 = 46;
-const int DigitalPin47 = 47;
-const int DigitalPin48 = 48;
-const int DigitalPin49 = 49;
-const int DigitalPin50 = 50;
-const int DigitalPin51 = 51;
-const int DigitalPin52 = 52;
-const int DigitalPin53 = 53;
-
-
-
-
-const int ThermistorTopFrontLeft = AnalogPin0;
-const int ThermistorTopFrontRight = AnalogPin1;
-const int ThermistorTopBackLeft = AnalogPin2;
-const int ThermistorTopBackRight = AnalogPin3;
-const int ThermistorBottomFrontLeft = AnalogPin4;
-const int ThermistorBottomFrontRight = AnalogPin5;
-const int ThermistorBottomBackLeft = AnalogPin6;
-const int ThermistorBottomBackRight = AnalogPin7;
-
-const int LightTopFrontLeft = DigitalPin46; 
-const int LightTopFrontRight = DigitalPin47; 
-const int LightTopBackLeft = DigitalPin48; 
-const int LightTopBackRight = DigitalPin49; 
-const int LightBottomFrontLeft = DigitalPin50; 
-const int LightBottomFrontRight = DigitalPin51; 
-const int LightBottomBackLeft = DigitalPin52; 
-const int LightBottomBackRight = DigitalPin53; 
-
-const int TemperatureMinusPin = DigitalPin45;
-const int TemperaturePlusPin = DigitalPin44;
-
-const int Accessories_Q7_Interrupt5_Pin18 = DigitalPin18;
-const int Digit1_Q4_Interrupt4_Pin19 = DigitalPin19;
-const int Digit2_Q5_Interrupt3_Pin20 = DigitalPin20;
-const int Digit2_Q6_Interrupt2_Pin21 = DigitalPin21;
+const int c_AnalogPin0 = 0;
+const int c_AnalogPin1 = 1;
+const int c_AnalogPin2 = 2;
+const int c_AnalogPin3 = 3;
+const int c_AnalogPin4 = 4;
+const int c_AnalogPin5 = 5;
+const int c_AnalogPin6 = 6;
+const int c_AnalogPin7 = 7;
+const int c_AnalogPin8 = 8;
+const int c_AnalogPin9 = 9;
+const int c_AnalogPin10 = 10;
+const int c_AnalogPin11 = 11;
+const int c_AnalogPin12 = 12;
+const int c_AnalogPin13 = 13;
+const int c_AnalogPin14 = 14;
+const int c_AnalogPin15 = 15;
+const int c_DigitalPin0 = 0;
+const int c_DigitalPin1 = 1;
+const int c_DigitalPin2 = 2;
+const int c_DigitalPin3 = 3;
+const int c_DigitalPin4 = 4;
+const int c_DigitalPin5 = 5;
+const int c_DigitalPin6 = 6;
+const int c_DigitalPin7 = 7;
+const int c_DigitalPin8 = 8;
+const int c_DigitalPin9 = 9;
+const int c_DigitalPin10 = 10;
+const int c_DigitalPin11 = 11;
+const int c_DigitalPin12 = 12;
+const int c_DigitalPin13 = 13;
+const int c_DigitalPin14 = 14;
+const int c_DigitalPin15 = 15;
+const int c_DigitalPin16 = 16;
+const int c_DigitalPin17 = 17;
+const int c_DigitalPin18 = 18;
+const int c_DigitalPin19 = 19;
+const int c_DigitalPin20 = 20;
+const int c_DigitalPin21 = 21;
+const int c_DigitalPin22 = 22;
+const int c_DigitalPin23 = 23;
+const int c_DigitalPin24 = 24;
+const int c_DigitalPin25 = 25;
+const int c_DigitalPin26 = 26;
+const int c_DigitalPin27 = 27;
+const int c_DigitalPin28 = 28;
+const int c_DigitalPin29 = 29;
+const int c_DigitalPin30 = 30;
+const int c_DigitalPin31 = 31;
+const int c_DigitalPin32 = 32;
+const int c_DigitalPin33 = 33;
+const int c_DigitalPin34 = 34;
+const int c_DigitalPin35 = 35;
+const int c_DigitalPin36 = 36;
+const int c_DigitalPin37 = 37;
+const int c_DigitalPin38 = 38;
+const int c_DigitalPin39 = 39;
+const int c_DigitalPin40 = 40;
+const int c_DigitalPin41 = 41;
+const int c_DigitalPin42 = 42;
+const int c_DigitalPin43 = 43;
+const int c_DigitalPin44 = 44;
+const int c_DigitalPin45 = 45;
+const int c_DigitalPin46 = 46;
+const int c_DigitalPin47 = 47;
+const int c_DigitalPin48 = 48;
+const int c_DigitalPin49 = 49;
+const int c_DigitalPin50 = 50;
+const int c_DigitalPin51 = 51;
+const int c_DigitalPin52 = 52;
+const int c_DigitalPin53 = 53;
 
 
-//digit reading from 30-37 in digital pins
-#endif
+
+
+const int c_ThermistorTopFrontLeft = c_AnalogPin0;
+const int c_ThermistorTopFrontRight = c_AnalogPin1;
+const int c_ThermistorTopBackLeft = c_AnalogPin2;
+const int c_ThermistorTopBackRight = c_AnalogPin3;
+const int c_ThermistorBottomFrontLeft = c_AnalogPin4;
+const int c_ThermistorBottomFrontRight = c_AnalogPin5;
+const int c_ThermistorBottomBackLeft = c_AnalogPin6;
+const int c_ThermistorBottomBackRight = c_AnalogPin7;
+
+const int c_LightTopFrontLeft = c_DigitalPin46; 
+const int c_LightTopFrontRight = c_DigitalPin47; 
+const int c_LightTopBackLeft = c_DigitalPin48; 
+const int c_LightTopBackRight = c_DigitalPin49; 
+const int c_LightBottomFrontLeft = c_DigitalPin50; 
+const int c_LightBottomFrontRight = c_DigitalPin51; 
+const int c_LightBottomBackLeft = c_DigitalPin52; 
+const int c_LightBottomBackRight = c_DigitalPin53; 
+
+const int c_TemperatureMinusPin = c_DigitalPin45;
+const int c_TemperaturePlusPin = c_DigitalPin44;
+
+const int c_AccessoriesTransitionPin = c_DigitalPin18;
+const int c_Digit1TransitionPin = c_DigitalPin19;
+const int c_Digit2TransitionPin = c_DigitalPin20;
+const int c_Digit3TransitionPin = c_DigitalPin21;
+
+const int c_ReadDigitPin37 = c_DigitalPin37;
+const int c_ReadDigitPin36 = c_DigitalPin36;
+const int c_ReadDigitPin35 = c_DigitalPin35;
+const int c_ReadDigitPin34 = c_DigitalPin34;
+const int c_ReadDigitPin33 = c_DigitalPin33;
+const int c_ReadDigitPin32 = c_DigitalPin32;
+const int c_ReadDigitPin31 = c_DigitalPin31;
+const int c_ReadDigitPin30 = c_DigitalPin30;
+
+
+const int c_SimpleLight = c_DigitalPin40;
+//**************************************************************************
+//
+//  REVISION HISTORY:
+//
+//
+//
+//**************************************************************************
