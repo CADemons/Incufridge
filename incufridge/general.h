fe9435b378df4bfe84f31fa2b8e7d34521a7e005
kiyun-k <kiywoo4ever@gmail.com>
2014-02-09 10:55:23 -0500

    Create general.h

---
+++ incufridge/general.h
@@ -0,0 +1,35 @@
+//**************************************************************************
+//
+//				COPYRIGHT NOTICE
+//			Copyright 2013 CONCORD ACADEMY
+//		This program is the property of CONCORD ACADEMY
+//		Any unauthorized use or duplication is prohibited
+//
+//**************************************************************************
+//
+//  Title		General Variables
+//  Filename		general.h
+//  Originator  	Kiyun Kim 
+//  Archive Location	(wherever on github)
+//
+//  Overview		Sets some project-wide variables.
+//
+//**************************************************************************
+
+#ifndef  GENERAL_H
+#define	 GENERAL_H
+
+const int c_LEVEL_HIGH = HIGH;
+const int c_LEVEL_LOW = LOW;
+
+
+
+//**************************************************************************
+//
+//  REVISION HISTORY:
+//
+//
+//
+//**************************************************************************
+
+#endif
