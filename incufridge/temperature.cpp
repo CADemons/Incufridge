9b63f496e1bfa81a7c42cd8e092afa7b2d597d20
kiyun-k <kiywoo4ever@gmail.com>
2014-05-18 10:46:03 -0400

    added comments

--- incufridge/temperature.cpp
+++ incufridge/temperature.cpp
@@ -1,17 +1,25 @@
-/*class Temperature{
-  public:
-  Temperature(int decisionpin, int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8);
-  int readDigit(int pins[8]);
-  float getTemperature(int digit1, int digit2, int digit3);
-  private:
-  int mydecisionpin, mypin1, mypin2, mypin3, mypin4, mypin5, mypin6, mypinn7, mypin8;
-  float myTemperature;
-  int myDigit1, myDigit2, myDigit3;
-  int myPins[8];
-};*/
+//**************************************************************************
+//
+//				COPYRIGHT NOTICE
+//			Copyright 2013 CONCORD ACADEMY
+//		This program is the property of CONCORD ACADEMY
+//		Any unauthorized use or duplication is prohibited
+//
+//**************************************************************************
+//
+//  Title		Temperature
+//  Filename		temperature.cpp
+//  Originator  	Kiyun Kim  
+//  Archive Location	https://github.com/CADemons/Incufridge/blob/master/incufridge/temperature.cpp//  Version		-
+//  Version		-
+//
+//  Overview		Allows the user to pull temperature data from thermistor
+//                      readings.
+//**************************************************************************
 
 #include "project.h"
 
+/* Constructor. */
 Temperature::Temperature(int decisionpin, int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8){
   int mydecisionpin = decisionpin;
   int mypin1 = pin1;
@@ -32,4 +40,10 @@ float Temperature::getTemperature(int digit1, int digit2, int digit3){
   myTemperature = float(digit1*10) + float(digit2) + digit3*0.1;
 }
 
-
+//**************************************************************************
+//
+//  REVISION HISTORY:
+//
+//
+//
+//**************************************************************************
