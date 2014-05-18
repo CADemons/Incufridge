c00fcd824e17e826fb142dbf9803bb3158d2ec07
kiyun-k <kiywoo4ever@gmail.com>
2014-05-18 10:51:11 -0400

    comments

--- incufridge/PWM.h
+++ incufridge/PWM.h
@@ -1,3 +1,22 @@
+//**************************************************************************
+//
+//				COPYRIGHT NOTICE
+//			Copyright 2013 CONCORD ACADEMY
+//		This program is the property of CONCORD ACADEMY
+//		Any unauthorized use or duplication is prohibited
+//
+//**************************************************************************
+//
+//  Title		PWM Class
+//  Filename		PWM.h
+//  Originator  	Kiyun Kim  
+//  Archive Location	https://github.com/CADemons/Incufridge/blob/master/incufridge/PWM.h
+//  Version		-
+//
+//  Overview		Provides framework for the PWM class, which allows the
+//                      user to toggle something on and off over a period of time.
+//**************************************************************************
+
 #ifndef PWM_H
 #define PWM_H
 
@@ -6,7 +25,7 @@ enum PwmState{
 };
 
 class Pwm{
-  public:
+public:
   Pwm(int pin, float periodSecs, float percentOn, float percentOff, PwmState state);
   static void PwmCommand(String* args);
   void Start();
@@ -16,7 +35,7 @@ class Pwm{
   static void updateAll();
   static Pwm* pwms[20];
   static int numPwms;
-  private:
+private:
   void ChangePolarity();
   int m_myPin;
   float m_myPeriodSecs;
@@ -34,3 +53,10 @@ class Pwm{
 };
 
 #endif
+//**************************************************************************
+//
+//  REVISION HISTORY:
+//
+//
+//
+//**************************************************************************
