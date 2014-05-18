0e71c20fa30ea27323fdbe1326e8aaf6cfe04173
kiyun-k <kiywoo4ever@gmail.com>
2014-05-18 10:48:14 -0400

    comments

--- incufridge/incufridge.ino
+++ incufridge/incufridge.ino
@@ -1,8 +1,26 @@
+//**************************************************************************
+//
+//				COPYRIGHT NOTICE
+//			Copyright 2013 CONCORD ACADEMY
+//		This program is the property of CONCORD ACADEMY
+//		Any unauthorized use or duplication is prohibited
+//
+//**************************************************************************
+//
+//  Title		Incufridge
+//  Filename		incufridge.ino
+//  Originator  	Kiyun Kim
+//  Archive Location	https://github.com/CADemons/Incufridge/blob/master/incufridge/incufridge.ino
+//  Version		-
+//
+//  Overview		Pulls everything together to run the incufridge.
+//                      
+//**************************************************************************
+
 #include "project.h"
 
-//Change display mode without changing temperature
+/* ButtonHoldTime is the time to change display mode without changing temperature */
 const int ButtonHoldTime = 2200;
-//Button light(SimpleLight, ButtonHoldTime, 1);
 Pwm myPwm = Pwm(7, 6, 0.5, 0.5, PWM_LOW);
 CommandProcessor processor (';', 60, 0);
 boolean recieving = false;
@@ -15,42 +33,62 @@ SevenSegments Display = {
 };
 
 ConversionPattern ConversionPatternTens[10] = {
-  {"11001111", "0"}, {"00000110", "1"}, {"10101011", "2"}, {"00101111", "3"},
-  {"01100110", "4"}, {"01101101", "5"}, {"11101101", "6"}, {"00000111", "7"},
-  {"11101111", "8"}, {"01100111", "9"}
+  {"11001111", "0"}
+  , {"00000110", "1"}
+  , {"10101011", "2"}
+  , {"00101111", "3"}
+  , {"01100110", "4"}
+  , {"01101101", "5"}
+  , {"11101101", "6"}
+  , {"00000111", "7"}
+  , {"11101111", "8"}
+  , {"01100111", "9"}
 };
 
 
 ConversionPattern ConversionPatternOnes[10] = {
-  {"11011111", "0."}, {"00010110", "1."}, {"10111011", "2."}, {"00111111", "3."},
-  {"01110110", "4."}, {"01111101", "5."}, {"11111101", "6."}, {"00010111", "7."},
-  {"11111111", "8."}, {"01110111", "9."}
+  {"11011111", "0."}
+  , {"00010110", "1."}
+  , {"10111011", "2."}
+  , {"00111111", "3."}
+  , {"01110110", "4."}
+  , {"01111101", "5."}
+  , {"11111101", "6."}
+  , {"00010111", "7."}
+  , {"11111111", "8."}
+  , {"01110111", "9."}
 };
 
 ConversionPattern ConversionPatternDecimals[10] = {
-  {"11001111", "0"}, {"00000110", "1"}, {"10101011", "2"}, {"00101111", "3"},
-  {"01100110", "4"}, {"01101101", "5"}, {"11101101", "6"}, {"00000111", "7"},
-  {"11101111", "8"}, {"01100111", "9"}
+  {"11001111", "0"}
+  , {"00000110", "1"}
+  , {"10101011", "2"}
+  , {"00101111", "3"}
+  , {"01100110", "4"}
+  , {"01101101", "5"}
+  , {"11101101", "6"}
+  , {"00000111", "7"}
+  , {"11101111", "8"}
+  , { "01100111", "9"}
 };
 
 
 
 LEDReader Reader = LEDReader(Display, c_Digit1TransitionPin, c_Digit2TransitionPin,
-                             c_Digit3TransitionPin, ConversionPatternTens, ConversionPatternOnes, ConversionPatternDecimals);
+c_Digit3TransitionPin, ConversionPatternTens, ConversionPatternOnes, ConversionPatternDecimals);
 
 int PreviousTime, CurrentTime, TimeElapsed;
 boolean EnoughTimeElapsed = false;
 
 
-
+/* Sets up various functions--PWM characteristics and the LED Display Reader 
+ initialized, the Command Processer is given a list of commands, and Pins are set. */
 void setup() {
-  //  pinMode(3, OUTPUT);
   myPwm.Start();
   Serial.begin(9600);
 
   Light m_Light(c_SimpleLight);
 
-  processor.AddCommand(&go, "GO");
   processor.AddCommand(&Pwm::PwmCommand, "PWM");
   processor.AddCommand(&Fan::FanOn, "FAN_ON");
   processor.AddCommand(&Fan::FanOff, "FAN_OFF");
@@ -60,37 +98,22 @@ void setup() {
   processor.AddCommand(&PressButton, "PRESS_BUTTON");
   processor.AddCommand(&SetTemp, "SET_TEMP");
 
-  //  pinMode(SimpleLight, OUTPUT);
   Reader.Initialize();
   PreviousTime = 0;
 }
+
+/* Runs the program. */
 void loop() {
-
-
-  /*CurrentTime = millis();
-  TimeElapsed = CurrentTime - PreviousTime;
-  if(TimeElapsed > 5000){
-    EnoughTimeElapsed = true;
-  }
-  if(EnoughTimeElapsed){
-    Serial.println(Reader.ShowDisplay());
-    EnoughTimeElapsed = false;
-    PreviousTime = CurrentTime;
-  }*/
-
-
-  //  light.Press();
-  //Pwm::updateAll();
-  // digitalWrite(3, HIGH);
   if (!recieving) {
     Serial.println('A');
     delay(1000);
   }
   myPwm.Update();
   delay(100);
-  // digitalWrite(3, LOW);
+
 }
 
+/* Processes a command input by the user. */
 void serialEvent() {
   if (!recieving) {
     int checks = 0;
@@ -107,35 +130,26 @@ void serialEvent() {
       }
       Serial.println(checks);
       Serial.println('R');
-    } else {
+    } 
+    else {
       Serial.println("Processing command");
       delay(100);
       int count = Serial.readBytesUntil('\0', inbytes, 30);
       processor.ProcessCharacter(readChar);
-      //Serial.print(readChar);
       for (int i = 0; i < count; i++) {
-        //Serial.print(inbytes[i]);
         processor.ProcessCharacter(inbytes[i]);
       }
-      //Serial.println();
     }
     recieving = false;
   }
 }
 
-void go(String* args) {
-  Serial.println("gone");
-  for (int i = 0; i < 10; i++) {
-    if (!args[i].equals("")) {
-      Serial.println(args[i]);
-    }
-  }
-}
-
+/* Reads the LED Display as a string. */
 void ReadDisplay(String* args) {
   Serial.println(Reader.ShowDisplay());
 }
 
+/* Presses a button. */
 void PressButton(String* args) {
   char arg0[10];
   char arg1[10];
@@ -146,6 +160,7 @@ void PressButton(String* args) {
   PressButton(pin, hold);
 }
 
+/* Presses a button for a set amount of time. */
 void PressButton(int pin, int time) {
   Serial.println(time);
   Button button(pin, time, c_LEVEL_LOW);
@@ -153,6 +168,7 @@ void PressButton(int pin, int time) {
   button.HoldFor(time);
 }
 
+/* Sets the target temperature to the user-input temperature. */
 void SetTemp(String* args) {
   char arg0[10];
   args[0].toCharArray(arg0, 10);
@@ -167,17 +183,16 @@ void SetTemp(String* args) {
       delay(200);
       Button button(44, 0, c_LEVEL_LOW);
       button.Initialize();
-      //button.HoldFor(ButtonHoldTime);
       while (target < GetDisplayFloat()) {
         button.HoldFor(100);
         delay(100);
       }
       button.Release();
-    } else {
+    } 
+    else {
       delay(200);
       Button button(45, 0, c_LEVEL_LOW);
       button.Initialize();
-      //button.HoldFor(ButtonHoldTime);
       while (target > GetDisplayFloat()) {
         button.HoldFor(100);
         delay(100);
@@ -187,53 +202,22 @@ void SetTemp(String* args) {
   }
 }
 
+/* Gets the number on the display as a float. */
 float GetDisplayFloat() {
   char buf[10];
   Reader.ShowDisplay().toCharArray(buf, 10);
   return atof(buf);
 }
-/* for integration with GUI
-#include "project.h"
-const int ButtonHoldTime = 250;
-int temps[12];
-boolean recieving=false;
-Button tempminus(TemperatureMinusPin, ButtonHoldTime);
-
- Pwm myPwm = Pwm(3, 5, 0.8, 0.2, PWM_LOW);
-void setup(){
-  pinMode(3, OUTPUT);
-  myPwm.Start();
-  Serial.begin(9600);
 
 
-}
-void loop(){
-  myPwm.Update();
-  delay(1000);
-  if(!recieving){
-  Serial.println('A');
-  }
- // digitalWrite(3, HIGH);
-  //delay(1000);
- // digitalWrite(3, LOW);
-  //delay(1000);
-}
+//**************************************************************************
+//
+//  REVISION HISTORY:
+//
+//
+//
+//**************************************************************************
 
-//Do not rename this function!
-void serialEvent(){
-  int checks=0;
-  char inbytes[30];
-  recieving=true;
-  Serial.flush();
-  if(Serial.read() == 'B'){
-    delay(2000);
-    Serial.readBytesUntil('D',inbytes,30);
-    for(int a=0; a!=12; a++){
-      temps[a] = inbytes[a];
-      checks += temps[a];
-    }
-    Serial.println(checks);
-    Serial.println('R');
-    recieving=false;
-  }
-}*/
+
+
+
