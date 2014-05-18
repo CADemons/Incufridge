14a66ff5040732b0ef048eda6b212cbf5d9af9e0
kiyun-k <kiywoo4ever@gmail.com>
2014-05-18 10:49:23 -0400

    comments

--- incufridge/fan.cpp
+++ incufridge/fan.cpp
@@ -7,33 +7,36 @@
 //
 //**************************************************************************
 //
-//  Title		- Fan class
-//  Filename		- fan.cpp
-//  Originator  	-  
-//  Archive Location	-
+//  Title		Fan class
+//  Filename		fan.cpp
+//  Originator  	Kiyun Kim 
+//  Archive Location	https://github.com/CADemons/Incufridge/blob/master/incufridge/fan.cpp
+//  Version		-
 //  Version		-
 //
-//  Overview		- Provides ability to turn fan on and off
+//  Overview		Provides ability to turn fan on and off
 //
 //**************************************************************************
 
 #include "project.h"
 
+/* Constructor */
 Fan::Fan(int pin, PwmState state) {
   m_pin = pin;
   m_state = state;
 }
 
-//Turn the fan on
+/* Turn the fan on. */
 void Fan::FanOn(String* commandIn) {
   Serial.println("Fan On");
 }
 
-//Turn the fan off
+/* Turn the fan off. */
 void Fan::FanOff(String* commandIn) {
   Serial.println("Fan Off");
 }
 
+/* Sets the speed of the fan. */
 void Fan::FanSpeed() {
   
 }
